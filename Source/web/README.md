ed4e119be4f935d3aa397f672827d79c5a2f8477
Devin503 <devinpschweiss@gmail.com>
2022-03-20 11:55:55 -0400

    add html5 error thing

---
+++ Source/web/README.md
@@ -0,0 +1,7 @@
+# Web folder
+
+This folder contains any assets needed for web builds of the application.
+
+### WebError.hx
+
+This is the equivalent of the desktop exceptions. It's pretty much the "We can't continue, you should refresh." screen.
---
+++ Source/web/WebError.hx
@@ -0,0 +1,33 @@
+package web;
+
+import flixel.FlxG;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
+import PathFinder;
+
+using StringTools;
+
+/**This is pretty much the "We can't continue" screen. If you see this on the GitHub.io page, just click the "Create an issue..." button and select "Weather App"
+    @since v0.0.3*/
+class WebError extends FlxSubState {
+    var errorTxt:FlxText;
+    var errorBg:FlxSprite;
+    var err_Msg:String = "Oh no! Something's gone wrong.\n\nWe've encountered a fatal error and cannot continue.\nIf you're on the GitHub.io page, click on the\n\"Create an issue...\" button and select \"Weather App\".\nPlease include a screenshot of this error.\n\n";
+
+    public function new(errorMsg:String) {
+        super();
+        err_Msg += errorMsg;
+    }
+
+    override function create() {
+        FlxG.sound.play(PathFinder.sound('errorDoh'));
+        errorBg = new FlxSprite(0).makeGraphic(FlxG.width, FlxG.height, 0xFF000000);
+        add(errorBg);
+
+        errorTxt = new FlxText(0, 0, 0, err_Msg, 16);
+        errorTxt.screenCenter();
+        errorTxt.scrollFactor.set();
+        add(errorTxt);
+    }
+}
