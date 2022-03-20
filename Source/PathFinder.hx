0a43482759a6c4cb998b6ad2cad55ba3f1fd56aa
Devin503 <devinpschweiss@gmail.com>
2022-03-19 20:16:00 -0400

    add a sparrow atlas function

--- Source/PathFinder.hx
+++ Source/PathFinder.hx
@@ -4,7 +4,7 @@ import haxe.Json;
 import SusUtil;
 import PogTools;
 import sys.FileSystem;
-
+import flixel.graphics.frames.FlxAtlasFrames as FunnyFrames;
 using StringTools;
 
 /**Simple class to get certain files and check the existence of them as well.
@@ -15,13 +15,14 @@ class PathFinder {
     static inline final WxIconPath_Day = 'Assets/Icons/weather/64x64/day/';
     static inline final WxIconPath_Night = 'Assets/Icons/weather/64x64/night/';
     static inline final ImagePath = 'Assets/Images/';
+    static inline final AudioExt = #if web '.mp3' #else '.ogg' #end;
     /**Grabs a weather icon if you have the direct path to it in the application's files.
         @param key Path to the icon
         @returns key if the file exists, 'not found' if it does not.
         @since v0.0.2*/
     public static function iconDirect(key:String):String {
-        if (FileSystem.exists(key)) 
-            return key;
+        if (FileSystem.exists(key + '.png')) 
+            return key + '.png';
         return 'not found';
     }
     /**Grab a sound effect to play. This function is planned to be used in ForecastState in case of certain severe weather alerts.
@@ -31,9 +32,18 @@ class PathFinder {
         @since v0.0.2*/
     public static function sound(key:String, ?directPath:String) {
         if (directPath != null) {
-            if (exists(directPath + key)) return directPath + key;
+            if (exists(directPath + key + AudioExt)) return directPath + key + AudioExt;
         } else {
-            if (exists(AudioPath + key)) return AudioPath + key;
+            if (exists(AudioPath + key + AudioExt)) return AudioPath + key + AudioExt;
+        }
+        return 'not found';
+    }
+    /**If you want to pull a funny prank, just create a louder version of an existing sound and use this function to play it.*/
+    public static function loud_sound(key:String, ?directPath:String) {
+        if (directPath != null) {
+            if (exists(directPath + key + '-loud' + AudioExt)) return directPath + key + '-loud' + AudioExt;
+        } else {
+            if (exists(AudioPath + key + '-loud' + AudioExt)) return AudioPath + key + '-loud' + AudioExt;
         }
         return 'not found';
     }
@@ -44,9 +54,9 @@ class PathFinder {
         @since v0.0.2*/
     public static function image(key:String, ?directPath:String) {
         if (directPath != null) {
-            if (exists(directPath + key)) return directPath + key;
+            if (exists(directPath + key + '.png')) return directPath + key + '.png';
         } else {
-            if (exists(ImagePath + key)) return ImagePath + key;
+            if (exists(ImagePath + key + '.png')) return ImagePath + key + '.png';
         }
         return 'not found';
     }
@@ -56,9 +66,21 @@ class PathFinder {
         @param variant Day or night
         @since v0.0.2*/
     public static function getIcon(icon:String, variant:String) {
-        if (variant == 'day' && exists(WxIconPath_Day + icon)) return WxIconPath_Day + icon;
-        else if (variant == 'night' && exists(WxIconPath_Night + icon)) return WxIconPath_Night + icon;
+        if (variant == 'day' && exists(WxIconPath_Day + icon + '.png')) return WxIconPath_Day + icon + '.png';
+        else if (variant == 'night' && exists(WxIconPath_Night + icon + '.png')) return WxIconPath_Night + icon + '.png';
         return 'not found';
+    }
+    /**Get the Sparrow atlas of a spritesheet.
+        @param key The file name. This is used for both the png and xml files.
+        @param directPath (Optional) The direct path. Don't add the file name, just the path leading up to it (plus a '/' at the end)
+        @since v0.0.2*/
+    public static function birdAtlas(key:String, ?directPath:String) {
+        if (directPath != null) {
+            if (exists(directPath + key + '.png') && exists(directPath + key + '.xml')) return FunnyFrames.fromSparrow(PathFinder.image(key, directPath), directPath + key + '.xml');
+        } else {
+            if (exists(ImagePath + key + '.png') && exists(ImagePath + key + '.xml')) return FunnyFrames.fromSparrow(PathFinder.image(key), ImagePath + key + '.xml');
+        }
+        return FunnyFrames.fromSparrow('Assets/Images/speen.png', 'Assets/Images/speen.xml');
     }
     /**Check if a file exists.
         @param key Path to a file.
