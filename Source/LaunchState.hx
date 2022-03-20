b064dc43851a40e0de387f16b8ec8eb140e87832
Devin503 <devinpschweiss@gmail.com>
2022-03-19 20:15:33 -0400

    add setup stuff and switch icon to wind

--- Source/LaunchState.hx
+++ Source/LaunchState.hx
@@ -1,5 +1,7 @@
 package;
 
+import settings.InitialSetup;
+import flixel.util.FlxTimer;
 import haxe.Http;
 import openfl.Lib;
 import flixel.FlxG;
@@ -12,17 +14,19 @@ import flixel.ui.FlxBar;
 import flixel.ui.FlxButton;
 import flixel.text.FlxText;
 import SusUtil;
+import util.ACSpinner;
 
 using StringTools;
 
 /**The launch state. This contains variables used throughout the application, such as the icon theme.
 @since v0.0.1 (March 2022)*/
 class LaunchState extends FlxState {
-    public static var iconTheme:String = ''; // I STILL NEED TO SET UP ICON THEMES.
+    public static var windUnits:String = '';
     public static var temperatureUnits:String = ''; // THIS GETS SET IN THE SAVE DATA AFTER INITIAL SETUP, THOUGH THERE'LL ALSO BE A SETTINGS STATE.
     var gameCamera:FlxCamera;
     var appVersionInfo:String = 'HaxeFlixel Weather App - Made by devin503 - v';
     var loadingText:FlxText;
+    var spin:ACSpinner;
 
     public function new() {
         super();
@@ -49,13 +53,22 @@ class LaunchState extends FlxState {
         loadingText = new FlxText(0, FlxG.height - 18, 0, "Please wait...", 16);
         add(loadingText);
 
+        spin = new ACSpinner(FlxG.width - 50, FlxG.height - 50);
+        spin.spin();
+        add(spin);
+
         #if debug
         //APIShit.getNow('Myrtle Beach');
         trace('among us');
         #end
 
         //SusUtil.getWeatherIcon('night/420.png');
-        ForecastState.location = APIShit.getForecast('Day Heights, Ohio');
-        openSubState(new forecasts.HourlyForecastSubstate());
+        new FlxTimer().start(3, function(tmr:FlxTimer) {
+            if (!FlxG.save.data.finishedSetup) FlxG.switchState(new InitialSetup()) else {
+                temperatureUnits = FlxG.save.data.tempUnits[0];
+                windUnits = FlxG.save.data.tempUnits[1];
+                openSubState(new WeatherSearch());
+            }
+        });
     }
 }
