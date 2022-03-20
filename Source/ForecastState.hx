17099a8b6564ba44656b5aa9c4fc03686c577831
Devin503 <devinpschweiss@gmail.com>
2022-03-19 21:11:53 -0400

    add day forecast

--- Source/ForecastState.hx
+++ Source/ForecastState.hx
@@ -11,6 +11,7 @@ import flixel.addons.ui.FlxUI;
 import flixel.ui.FlxButton;
 import flixel.text.FlxText;
 import FlxUIDropDownMenuCustom;
+import util.WeatherIcon;
 
 class ForecastState extends FlxState {
     public static var location:ResponseForecast;
@@ -53,6 +54,9 @@ class ForecastState extends FlxState {
         add(new FlxText(0, 0, 0, 'WORK IN PROGRESS, PRESS ESC TO EXIT!'));
         // funcs go here but i need to make them first.
         addAlertUI();
+        addAstroUI();
+        addTodayUI();
+        ForecastUI.selected_tab_id = 'Today';
     }
 
     override function update(elapsed:Float) {
@@ -64,6 +68,7 @@ class ForecastState extends FlxState {
     var alertDropDown:FlxUIDropDownMenuCustom;
     var at_Event:FlxText;
     var at_areas:FlxText;
+    var at_desc:FlxText;
     function addAlertUI() {
         alertThing = new FlxUI(null, ForecastUI);
         alertThing.name = 'Alerts';
@@ -73,14 +78,17 @@ class ForecastState extends FlxState {
         });
         reloadAlertDropDown(); // THIS WILL PUT THE EVENT NAMES IN!
 
-        at_Event = new FlxText(alertDropDown.x, alertDropDown.y + 18, 0, 'Pick an alert first.', 8);
+        at_Event = new FlxText(alertDropDown.x, alertDropDown.y + 30, 0, 'Pick an alert first.', 12);
 
-        at_areas = new FlxText(at_Event.x, at_Event.y + 10, 0, 'N/A', 8);
+        at_areas = new FlxText(at_Event.x, at_Event.y + 14, 0, 'N/A', 12);
+
+        at_desc = new FlxText(at_areas.x, at_areas.y + 14, 0, 'No description', 12);
 
         alertThing.add(new FlxText(alertDropDown.x, alertDropDown.y - 18, 0, 'Select an alert:', 8));
-        alertThing.add(alertDropDown);
         alertThing.add(at_Event);
         alertThing.add(at_areas);
+        alertThing.add(at_desc);
+        alertThing.add(alertDropDown);
         ForecastUI.addGroup(alertThing);
     }
 
@@ -101,6 +109,35 @@ class ForecastState extends FlxState {
         var moonPhase = new FlxText(15, moonsText.y + 20, 0, 'Moon phase: ' + astron[4]);
 
         var moonIllum = new FlxText(15, moonPhase.y + 20, 0, 'Moon illumination: ' + astron[5], 16);
+
+        astroThing.add(sunrText);
+        astroThing.add(sunsText);
+        astroThing.add(moonrText);
+        astroThing.add(moonsText);
+        astroThing.add(moonPhase);
+        astroThing.add(moonIllum);
+        ForecastUI.addGroup(astroThing);
+    }
+
+    function addTodayUI() {
+        dayThing = new FlxUI(null, ForecastUI);
+        dayThing.name = 'Today';
+
+        var wxIcon:WeatherIcon = new WeatherIcon(15, 30, fc.day.condition.icon, SusUtil.getCurrentHour());
+
+        var conditionText = new FlxText(wxIcon.x + 69, wxIcon.y + 10, 0, fc.day.condition.text, 8);
+
+        if (LaunchState.temperatureUnits == 'F') {
+            var tempText = new FlxText(wxIcon.x + 69, conditionText.y + 12, 0, fc.day.maxtemp_f + '/' + fc.day.mintemp_f, 8);
+            dayThing.add(tempText);
+        } else {
+            var tempText = new FlxText(wxIcon.y + 69, conditionText.y + 12, 0, fc.day.maxtemp_c + '/' + fc.day.mintemp_c, 8);
+            dayThing.add(tempText);
+        }
+
+        dayThing.add(wxIcon);
+        dayThing.add(conditionText);
+        ForecastUI.addGroup(dayThing);
     }
 
     function loadAlert(Alert:WeatherAlert) {
@@ -112,6 +149,9 @@ class ForecastState extends FlxState {
 
         at_areas.text = alertValues[4];
         at_areas.fieldWidth = 0;
+
+        at_desc.text = alertValues[11];
+        at_desc.fieldWidth = 0;
     }
     function reloadAlertDropDown() {
         var droplist:Array<String> = [];
