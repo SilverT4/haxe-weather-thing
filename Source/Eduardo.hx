5b43f7c99e4d291b4a8539ed45da842f8bc5b39e
Devin503 <devinpschweiss@gmail.com>
2022-03-19 21:12:38 -0400

    change positioning

--- Source/Eduardo.hx
+++ Source/Eduardo.hx
@@ -14,7 +14,7 @@ class Eduardo extends FlxSprite {
             doinJumpscare = true;
             scale.scale(5);
             updateHitbox();
-            setPosition(0, 150);
+            setPosition(0, 0);
             screenCenter(X);
             animation.play('wellWellWell');
             flixel.FlxG.sound.play(PathFinder.loud_sound('theFunnyWell'), 1, false, null, true, function() {
@@ -23,15 +23,30 @@ class Eduardo extends FlxSprite {
         }
     }
 
+    public function spawnJumpscare() {
+        if (animation.getByName('wellWellWell') != null) {
+            doinJumpscare = true;
+            scale.scale(5);
+            updateHitbox();
+            setPosition(0, 0);
+            screenCenter(X);
+            animation.play('wellWellWell');
+            flixel.FlxG.sound.play(PathFinder.loud_sound('theFunnyWell'), 1, false, null, true, function() {
+                throw new haxe.Exception('You have been WELL WELL WELL\'D!\n\nSend this to a friend to totally WELL WELL WELL them!');
+            });
+        }
+        return this;
+    }
     public function new(x:Float, y:Float) {
         super(x, y);
         frames = PathFinder.birdAtlas('Eduardo');
         animation.addByPrefix('idle', 'EduardoIdle', 24, false);
         animation.addByIndices('wellWellWell', 'EduardoWell', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], '', 24, false);
+        animation.play('idle');
     }
 
     public function dance() {
-        if (animation.getByName('idle') != null && !doinJumpscare) {
+        if (animation.curAnim.finished && !doinJumpscare) {
             animation.play('idle');
         }
     }
