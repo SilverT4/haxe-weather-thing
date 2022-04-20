44b2055814e5980e49f57271e4ead10b8620f909
Devin503 <devinpschweiss@gmail.com>
2022-04-20 16:45:15 -0400

    copy dominantColor from FNF coolutil

--- Source/PogTools.hx
+++ Source/PogTools.hx
@@ -84,4 +84,29 @@ class PogTools {
         return [Alert.headline, Alert.msgType, Alert.severity, Alert.urgency, Alert.areas.split('; ').join(', '), Alert.category, Alert.certainty, Alert.event, Alert.note, Alert.effective, Alert.expires, Alert.desc, Alert.instruction]; //BASIC
     }
     
+    public static function domColor(sprite:flixel.FlxSprite):Int{
+		var countByColor:Map<Int, Int> = [];
+		for(col in 0...sprite.frameWidth){
+			for(row in 0...sprite.frameHeight){
+			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+			  if(colorOfThisPixel != 0){
+				  if(countByColor.exists(colorOfThisPixel)){
+				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+					 countByColor[colorOfThisPixel] = 1;
+				  }
+			  }
+			}
+		 }
+		var maxCount = 0;
+		var maxKey:Int = 0;//after the loop this will store the max color
+		countByColor[flixel.util.FlxColor.BLACK] = 0;
+			for(key in countByColor.keys()){
+			if(countByColor[key] >= maxCount){
+				maxCount = countByColor[key];
+				maxKey = key;
+			}
+		}
+		return maxKey;
+	}
 }
